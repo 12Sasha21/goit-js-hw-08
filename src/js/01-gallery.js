2273d40f5acaece07e5628ed1f712db9de94664a
12Sasha21 <90248554+12Sasha21@users.noreply.github.com>
2022-01-29 16:14:37 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,13 +5,8 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 const gallery = document.querySelector('.gallery');
-const galleryMarkup = createImgGallery(galleryItems);
-
+const galleryMarkup = imgCardTpl(galleryItems);
 gallery.insertAdjacentHTML('beforeend', galleryMarkup);
-
-function createImgGallery(galleryItems) {
-  return imgCardTpl(galleryItems);
-}
 
 new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
